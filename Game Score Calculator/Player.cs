9028b740ff4c89ab39f3628c40f2aa2cbd0c91e7
Lindsay Alexander <linmalex@gmail.com>
2018-08-09 15:08:46 -0700

    added constructor chain to player class

--- Game Score Calculator/Player.cs
+++ Game Score Calculator/Player.cs
@@ -8,29 +8,22 @@ namespace Game_Score_Calculator
 {
     class Player
     {
-        public Player(string name, int playerScore = 0)
+        public Player(string name) : this(name, 0)
         {
+
+        }
+        public Player(string name, int startingScore)
+        {
+            Score = startingScore;
             Name = name;
+            IsWinner = false;
         }
 
         //properties
         public int Score { get; set; }
         public string Name { get; set; }
-        public bool IsActivelyPlaying { get; set; }
-        public bool Stay { get; set; }
         public bool IsWinner { get; set; }
 
-
-        //overloading operators
-        public static Game operator +(Game game, Player player)
-        {
-            game.Players.Add(player);
-            return game;
-        }
-        public static Game operator -(Game game, Player player)
-        {
-            game.Players.Remove(player);
-            return game;
-        }
     }
 }
+
