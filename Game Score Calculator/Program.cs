609e7873e0fb804f5659dbba486133d423be605e
Lindsay Alexander <linmalex@gmail.com>
2018-08-09 15:09:08 -0700

    tidied up code

--- Game Score Calculator/Program.cs
+++ Game Score Calculator/Program.cs
@@ -10,21 +10,17 @@ namespace Game_Score_Calculator
     {
         static void Main(string[] args)
         {
-            //Add player names to list
             List<Player> players = new List<Player>();
-            List<Player> finalists = new List<Player>();
             Console.WriteLine("Please enter the number of players");
             int numPlayers = Convert.ToInt32(Console.ReadLine());
-            for (int i=0; i <numPlayers; i++)
+            for (int i=0; i < numPlayers; i++)
             {
                 Console.WriteLine("Please enter the name of a player");
                 string name = Console.ReadLine();
                 Player player = new Player(name);
                 players.Add(player);
             }
-            //set maxScore value
             int maxScore = 0;
-
             int counter = 1;
             while (maxScore < 17)
             {
@@ -36,38 +32,30 @@ namespace Game_Score_Calculator
                 Console.WriteLine("Which player was Frost Bitten?");
                 string frostBitten = Console.ReadLine();
                 
-                for (int j = 0; j < numPlayers; j++)
+                for (int i = 0; i < numPlayers; i++)
                 {
-                    Console.WriteLine("How many fool cards did {0} have?", players[j].Name);
+                    Console.WriteLine("How many fool cards did {0} have?", players[i].Name);
                     int foolCount = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine("How many okus tokens did {0} have?", players[j].Name);
+                    Console.WriteLine("How many okus tokens did {0} have?", players[i].Name);
                     int okusCount = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine("How many luminary cards did {0} have?", players[j].Name);
+                    Console.WriteLine("How many luminary cards did {0} have?", players[i].Name);
                     int luminaryCount = Convert.ToInt32(Console.ReadLine());
-
-                    if (bumperCrop == players[j].Name)
+                    if (bumperCrop == players[i].Name)
                     {
-                        players[j].Score += 4;
+                        players[i].Score += 4;
                     }
-                    if (sunKissed == players[j].Name)
+                    if (sunKissed == players[i].Name)
                     {
-                        players[j].Score += 2;
+                        players[i].Score += 2;
                     }
-                    if (frostBitten == players[j].Name)
+                    if (frostBitten == players[i].Name)
                     {
-                        players[j].Score -= 2;
+                        players[i].Score -= 2;
                     }
-                    players[j].Score += foolCount + okusCount + luminaryCount;
-
-                    Console.WriteLine("{0}'s score is {1}. ", players[j].Name, players[j].Score);
+                    players[i].Score += foolCount + okusCount + luminaryCount;
+                    Console.WriteLine("{0}'s score is {1}. ", players[i].Name, players[i].Score);
                 }
-                maxScore= players.Max(x => x.Score);
-                Console.WriteLine("The current max score is {0}", maxScore);
-                counter++;
             }
-            
-            
-            
             if(maxScore >= 17)
             {
                 foreach (Player player in players)
